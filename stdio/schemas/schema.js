fac842d800680633df38ae7183efcce81588d298
robinbluelupin <thakurrobin344@gmail.com>
2022-03-22 23:55:16 +0530

    sacheme

--- stdio/schemas/schema.js
+++ stdio/schemas/schema.js
@@ -3,7 +3,14 @@ import createSchema from 'part:@sanity/base/schema-creator'
 
 // Then import schema types from any plugins that might expose them
 import schemaTypes from 'all:part:@sanity/base/schema-type'
-
+import works from './works'
+import testimonials from './testimonials'
+import brands from './brands'
+import abouts from './abouts'
+import experiences from './experiences'
+import skills from './skills'
+import workExperience from './workExperience'
+import contact from './contact'
 // We import object and document schemas
 import blockContent from './blockContent'
 import post from './post'
