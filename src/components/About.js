ccb2db8f40e40f6ed452d6dcf5b434ac91ca3e8e
robinbluelupin <thakurrobin344@gmail.com>
2022-04-17 16:57:01 +0530

    about us done

--- src/components/About.js
+++ src/components/About.js
@@ -10,15 +10,15 @@ function urlFor(source) {
 
 export default function About() {
   const [author, setAuthor] = useState(null);
-  const [experiences, setExperiences] = useState([]);
+  const [experiences, setworkExperience] = useState([]);
   const [skills, setSkills] = useState([]);
 
   useEffect(() => {
-    const query = '*[_type == "experiences"]';
+    const query = '*[_type == "workExperience"]';
     const skillsQuery = '*[_type == "skills"]';
 
     sanityClient.fetch(query).then((data) => {
-      setExperiences(data);
+      setworkExperience(data);
     });
 
     sanityClient.fetch(skillsQuery).then((data) => {
@@ -31,7 +31,9 @@ export default function About() {
         `*[_type == "author"]{
           name,
           "bio": bio[0].children[0].text,
-          "authorImage": image.asset->url
+          "authorImage": image.asset->url,
+          description,
+          longdescription
       }`
       )
       .then((data) => setAuthor(data[0]))
@@ -42,13 +44,36 @@ export default function About() {
 
   return (
     <main className="relative">
-      <img src={plumeria} alt="Plumeria Flower" className="absolute w-full" />
-      <div className="p-10 lg:pt-48 container mx-auto relative">
+      {/* <img src={plumeria} alt="Plumeria Flower" className="absolute w-full" /> */}
+
+      <div class="py-16 bg-white">
+        <div class="container m-auto px-6 text-gray-600 md:px-12 xl:px-6">
+          <div class="space-y-6 md:space-y-0 md:flex md:gap-6 lg:items-center lg:gap-12">
+            <div class="md:5/12 lg:w-5/12">
+              <img
+                src={urlFor(author.authorImage).url()}
+                alt="image"
+                loading="lazy"
+                width="1000"
+                height="1000" />
+            </div>
+            <div class="md:7/12 lg:w-6/12">
+              <h2 class="text-2xl text-gray-900 font-bold md:text-4xl"> Hey there. I'm{" "}
+                <span class="bg-indigo-600 text-white rounded px-1"> {author.name}</span>.</h2>
+              <p class="mt-6 text-gray-600">{author.bio}!</p>
+              <p class="text-gray-400 mt-4">{author.description}</p>
+              <p class="mt-6 text-gray-600">{author.longdescription}</p>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      {/* <div className="p-10 lg:pt-48 container mx-auto relative">
         <section className="bg-green-800 rounded-lg shadow-2xl lg:flex p-20">
           <img
             src={urlFor(author.authorImage).url()}
             className="rounded w-32 h-32 lg:w-64 lg:h-64 mr-8"
-            alt="Kapehe"
+            alt="robin"
           />
           <div className="text-lg flex flex-col justify-center">
             <h1 className="cursive text-6xl text-green-300 mb-4">
@@ -58,111 +83,73 @@ export default function About() {
             <p className="text-green-200 text-lg">{author.bio}</p>
           </div>
         </section>
-      </div>
-      {/* <section class="text-gray-600 body-font">
-          <div class="container px-5 py-24 mx-auto">
-            <div class="text-center mb-20">
-              <h1 class="sm:text-3xl text-2xl font-medium text-center title-font text-gray-900 mb-4">My skills</h1>
-              <p class="text-base leading-relaxed xl:w-2/4 lg:w-3/4 mx-auto">basicaliy working as full stack devloper </p>
-            </div>
-            <div class="flex flex-wrap lg:w-4/5 sm:mx-auto sm:mb-2 -mx-2">
-               {skills.map((skill) => (
-                  <div class="p-2 sm:w-1/2 w-full">
-                    <div class="bg-gray-100 rounded flex p-4 h-full items-center">
-                    <img class="text-gray-600 text-sm" src={urlFor(skill.icon)} alt={skill.name} />
-                      <span class="title-font font-medium">{skill.name}</span>
-                    </div>
-                  </div>
-                 ))}
-            </div>
+      </div> */}
+      <section class="text-gray-600 body-font">
+        <div class="container px-5 py-24 mx-auto">
+          <div class="text-center mb-20">
+            <h1 class="sm:text-3xl text-2xl font-medium text-center title-font text-gray-900 mb-4">My skills</h1>
+            {/* <p class="text-base leading-relaxed xl:w-2/4 lg:w-3/4 mx-auto">{author.description} </p> */}
+            <p class="text-base leading-relaxed xl:w-2/4 lg:w-3/4 mx-auto">{author.longdescription}</p>
           </div>
-        </section> 
-        <section class="text-gray-600 body-font">
-          <div class="container px-5 py-24 mx-auto flex flex-wrap">
-            <div class="flex relative pt-10 pb-20 sm:items-center md:w-2/3 mx-auto">
-              <div class="h-full w-6 absolute inset-0 flex items-center justify-center">
-                <div class="h-full w-1 bg-gray-200 pointer-events-none"></div>
-              </div>
-              <div class="flex-shrink-0 w-6 h-6 rounded-full mt-10 sm:mt-0 inline-flex items-center justify-center bg-indigo-500 text-white relative z-10 title-font font-medium text-sm">1</div>
-              <div class="flex-grow md:pl-8 pl-6 flex sm:items-center items-start flex-col sm:flex-row">
-                <div class="flex-shrink-0 w-24 h-24 bg-indigo-100 text-indigo-500 rounded-full inline-flex items-center justify-center">
-                  <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-12 h-12" viewBox="0 0 24 24">
-                    <path d="M12 22s8-4 8-10V5l-8-3-8 3v7c0 6 8 10 8 10z"></path>
-                  </svg>
-                </div>
-                <div class="flex-grow sm:pl-6 mt-6 sm:mt-0">
-                  <h2 class="font-medium title-font text-gray-900 mb-1 text-xl">Shooting Stars</h2>
-                  <p class="leading-relaxed">VHS cornhole pop-up, try-hard 8-bit iceland helvetica. Kinfolk bespoke try-hard cliche palo santo offal.</p>
+          <div class="flex flex-wrap lg:w-4/5 sm:mx-auto sm:mb-2 -mx-2">
+            {skills.map((skill) => (
+              <div class="p-2 sm:w-1/2 w-full">
+                <div class="bg-gray-100 rounded flex p-4 h-full items-center">
+                  <img class="text-gray-600 text-sm" src={urlFor(skill.icon)} alt={skill.name} />
+                  <span class="title-font font-medium">{skill.name}</span>
                 </div>
               </div>
-            </div>
-            <div class="flex relative pb-20 sm:items-center md:w-2/3 mx-auto">
-              <div class="h-full w-6 absolute inset-0 flex items-center justify-center">
-                <div class="h-full w-1 bg-gray-200 pointer-events-none"></div>
-              </div>
-              <div class="flex-shrink-0 w-6 h-6 rounded-full mt-10 sm:mt-0 inline-flex items-center justify-center bg-indigo-500 text-white relative z-10 title-font font-medium text-sm">2</div>
-              <div class="flex-grow md:pl-8 pl-6 flex sm:items-center items-start flex-col sm:flex-row">
-                <div class="flex-shrink-0 w-24 h-24 bg-indigo-100 text-indigo-500 rounded-full inline-flex items-center justify-center">
-                  <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-12 h-12" viewBox="0 0 24 24">
-                    <path d="M22 12h-4l-3 9L9 3l-3 9H2"></path>
-                  </svg>
-                </div>
-                <div class="flex-grow sm:pl-6 mt-6 sm:mt-0">
-                  <h2 class="font-medium title-font text-gray-900 mb-1 text-xl">The Catalyzer</h2>
-                  <p class="leading-relaxed">VHS cornhole pop-up, try-hard 8-bit iceland helvetica. Kinfolk bespoke try-hard cliche palo santo offal.</p>
-                </div>
-              </div>
-            </div>
-            <div class="flex relative pb-20 sm:items-center md:w-2/3 mx-auto">
-              <div class="h-full w-6 absolute inset-0 flex items-center justify-center">
-                <div class="h-full w-1 bg-gray-200 pointer-events-none"></div>
-              </div>
-              <div class="flex-shrink-0 w-6 h-6 rounded-full mt-10 sm:mt-0 inline-flex items-center justify-center bg-indigo-500 text-white relative z-10 title-font font-medium text-sm">3</div>
-              <div class="flex-grow md:pl-8 pl-6 flex sm:items-center items-start flex-col sm:flex-row">
-                <div class="flex-shrink-0 w-24 h-24 bg-indigo-100 text-indigo-500 rounded-full inline-flex items-center justify-center">
-                  <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-12 h-12" viewBox="0 0 24 24">
-                    <circle cx="12" cy="5" r="3"></circle>
-                    <path d="M12 22V8M5 12H2a10 10 0 0020 0h-3"></path>
-                  </svg>
-                </div>
-                <div class="flex-grow sm:pl-6 mt-6 sm:mt-0">
-                  <h2 class="font-medium title-font text-gray-900 mb-1 text-xl">The 400 Blows</h2>
-                  <p class="leading-relaxed">VHS cornhole pop-up, try-hard 8-bit iceland helvetica. Kinfolk bespoke try-hard cliche palo santo offal.</p>
-                </div>
-              </div>
-            </div>
-            <div class="flex relative pb-10 sm:items-center md:w-2/3 mx-auto">
-              <div class="h-full w-6 absolute inset-0 flex items-center justify-center">
-                <div class="h-full w-1 bg-gray-200 pointer-events-none"></div>
-              </div>
-              <div class="flex-shrink-0 w-6 h-6 rounded-full mt-10 sm:mt-0 inline-flex items-center justify-center bg-indigo-500 text-white relative z-10 title-font font-medium text-sm">4</div>
-              <div class="flex-grow md:pl-8 pl-6 flex sm:items-center items-start flex-col sm:flex-row">
-                <div class="flex-shrink-0 w-24 h-24 bg-indigo-100 text-indigo-500 rounded-full inline-flex items-center justify-center">
-                  <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-12 h-12" viewBox="0 0 24 24">
-                    <path d="M20 21v-2a4 4 0 00-4-4H8a4 4 0 00-4 4v2"></path>
-                    <circle cx="12" cy="7" r="4"></circle>
-                  </svg>
-                </div>
-                <div class="flex-grow sm:pl-6 mt-6 sm:mt-0">
-                  <h2 class="font-medium title-font text-gray-900 mb-1 text-xl">Neptune</h2>
-                  <p class="leading-relaxed">VHS cornhole pop-up, try-hard 8-bit iceland helvetica. Kinfolk bespoke try-hard cliche palo santo offal.</p>
-                </div>
-              </div>
-            </div>
+            ))}
           </div>
-        </section>*/}
-      {/* <section class="text-gray-600 body-font">
-        <h2 class="text-center mb-20">Latest projects</h2>
-          <div class="container px-5 py-24 mx-auto">
-          <h2 class="text-2xl font-semibold text-gray-800"></h2>
-            <div class="flex items-center lg:w-3/5 mx-auto border-b pb-10 mb-10 border-gray-200 sm:flex-row flex-col">
-              <h2 class="text-2xl font-semibold text-gray-800"></h2>
-              <div class="sm:w-32 sm:h-32 h-20 w-20 sm:mr-10 inline-flex items-center justify-center rounded-full bg-indigo-100 text-indigo-500 flex-shrink-0">
-                <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="sm:w-16 sm:h-16 w-10 h-10" viewBox="0 0 24 24">
-                  <path d="M22 12h-4l-3 9L9 3l-3 9H2"></path>
+        </div>
+      </section>
+      <section class="text-gray-600 body-font">
+        <h1 class="sm:text-3xl text-2xl font-medium text-center title-font text-gray-900 mb-4">Experiance</h1>
+        <div class="container px-5 py-24 mx-auto flex flex-wrap">
+         
+          {/* <h2 class=" flex relative pt-10 pb-20 sm:items-center font-medium title-font text-gray-900 mb-1 text-xl">{new Date(experience.date).toLocaleDateString()}</h2> */}
+          {experiences.map((experience) => (
+          <div class="flex relative pt-10 pb-20 sm:items-center md:w-2/3 mx-auto">
+            <div class="h-full w-6 absolute inset-0 flex items-center justify-center">
+              <div class="h-full w-1 bg-gray-200 pointer-events-none"></div>
+            </div>
+            {/* <div class="flex-shrink-0 w-6 h-6 rounded-full mt-10 sm:mt-0 inline-flex items-center justify-center bg-indigo-500 text-white relative z-10 title-font font-medium text-sm">1</div> */}
+            <div class="flex-grow md:pl-8 pl-6 flex sm:items-center items-start flex-col sm:flex-row">
+              <div class="flex-shrink-0 w-24 h-24 bg-indigo-100 text-indigo-500 rounded-full inline-flex items-center justify-center">
+                <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-12 h-12" viewBox="0 0 24 24">
+                  <path d="M12 22s8-4 8-10V5l-8-3-8 3v7c0 6 8 10 8 10z"></path>
                 </svg>
               </div>
-              {experiences.map((experience) => (
+              <div class="flex-grow sm:pl-6 mt-6 sm:mt-0">
+                <h2 class="font-medium title-font text-gray-900 mb-1 text-xl">{experience.profile}</h2>
+                {/* <p class="leading-relaxed"> {experience.date}</p> */}
+                
+                 <p class="leading-relaxed">{experience.desc}</p>
+                <p class="leading-relaxed"> {experience.company}</p>
+                <p class="leading-relaxed"> {experience.duration}</p>
+                {new Date(experience.To_date).toLocaleDateString()}
+                <p class="leading-relaxed"> {experience.name}</p>
+              </div>
+            </div>
+          </div>
+            ))}
+          {/* <div class="flex relative pb-20 sm:items-center md:w-2/3 mx-auto">
+          </div> */}
+        </div>
+        
+      </section>
+      {/* <section class="text-gray-600 body-font">
+        <h2 class="text-center mb-20">Latest projects</h2>
+        <div class="container px-5 py-24 mx-auto">
+          <h2 class="text-2xl font-semibold text-gray-800"></h2>
+          <div class="flex items-center lg:w-3/5 mx-auto border-b pb-10 mb-10 border-gray-200 sm:flex-row flex-col">
+            <h2 class="text-2xl font-semibold text-gray-800"></h2>
+            <div class="sm:w-32 sm:h-32 h-20 w-20 sm:mr-10 inline-flex items-center justify-center rounded-full bg-indigo-100 text-indigo-500 flex-shrink-0">
+              <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="sm:w-16 sm:h-16 w-10 h-10" viewBox="0 0 24 24">
+                <path d="M22 12h-4l-3 9L9 3l-3 9H2"></path>
+              </svg>
+            </div>
+            {experiences.map((experience) => (
               <div class="flex-grow sm:text-left text-center mt-6 sm:mt-0">
                 <h2 class="text-gray-900 text-lg title-font font-medium mb-2">{experience.profile}</h2>
                 <p class="leading-relaxed text-base">{experience.desc}.</p>
@@ -172,10 +159,10 @@ export default function About() {
                   </svg>
                 </a>
               </div>
-               ))}
-            </div>
-            </div>
-        </section> */}
+            ))}
+          </div>
+        </div>
+      </section> */}
     </main>
 
 
--- src/components/Contact.js
+++ src/components/Contact.js
@@ -34,49 +34,104 @@ export default function Contact() {
       //   .catch((err) => console.log(err));
     };
     return (
-        <>
-          <h2 className="head-text">Take a coffee & chat with me</h2>
+        // <>
+        //   <h2 className="head-text">Take a coffee & chat with me</h2>
     
-          <div className="app__footer-cards">
-            <div className="app__footer-card ">
-              <img src={email} alt="email" />
-              <a href="mailto:thakurrobin344@gmail.com" className="p-text">thakurrobin344@gmail.com</a>
-            </div>
-            <div className="app__footer-card">
-              <img src={mobile} alt="phone" />
-              <a href="tel:+91 8076911984 " className="p-text">+91 8076911984 </a>
-              <br></br>
-              <a href="tel:+91 7053631758 " className="p-text">+91 7053631758 </a>
-            </div>
-          </div>
-          {!isFormSubmitted ? (
-            <div className="app__footer-form app__flex">
-              <div className="app__flex">
-                <input className="p-text" type="text" placeholder="Your Name" name="username" value={username} onChange={handleChangeInput} />
-              </div>
-              <div className="app__flex">
-                <input className="p-text" type="email" placeholder="Your Email" name="email" value={email} onChange={handleChangeInput} />
-              </div>
-              <div>
-                <textarea
-                  className="p-text"
-                  placeholder="Your Message"
-                  value={message}
-                  name="message"
-                  onChange={handleChangeInput}
-                />
-              </div>
-              <button type="button" className="p-text" onClick={handleSubmit}>{!loading ? 'Send Message' : 'Sending...'}</button>
-            </div>
-          ) : (
-            <div>
-              <h3 className="head-text">
-                Thank you for getting in touch!
-              </h3>
-            </div>
-          )}
-        </>
+        //   <div className="app__footer-cards">
+        //     <div className="app__footer-card ">
+        //       <img src={email} alt="email" />
+        //       <a href="mailto:thakurrobin344@gmail.com" className="p-text">thakurrobin344@gmail.com</a>
+        //     </div>
+        //     <div className="app__footer-card">
+        //       <img src={mobile} alt="phone" />
+        //       <a href="tel:+91 8076911984 " className="p-text">+91 8076911984 </a>
+        //       <br></br>
+        //       <a href="tel:+91 7053631758 " className="p-text">+91 7053631758 </a>
+        //     </div>
+        //   </div>
+        //   {!isFormSubmitted ? (
+        //     <div className="app__footer-form app__flex">
+        //       <div className="app__flex">
+        //         <input className="p-text" type="text" placeholder="Your Name" name="username" value={username} onChange={handleChangeInput} />
+        //       </div>
+        //       <div className="app__flex">
+        //         <input className="p-text" type="email" placeholder="Your Email" name="email" value={email} onChange={handleChangeInput} />
+        //       </div>
+        //       <div>
+        //         <textarea
+        //           className="p-text"
+        //           placeholder="Your Message"
+        //           value={message}
+        //           name="message"
+        //           onChange={handleChangeInput}
+        //         />
+        //       </div>
+        //       <button type="button" className="p-text" onClick={handleSubmit}>{!loading ? 'Send Message' : 'Sending...'}</button>
+        //     </div>
+        //   ) : (
+        //     <div>
+        //       <h3 className="head-text">
+        //         Thank you for getting in touch!
+        //       </h3>
+        //     </div>
+        //   )}
+        // </>
 
-        
+
+<div class="h-screen md:flex">
+	<div
+		class="relative overflow-hidden md:flex w-1/2 bg-gradient-to-tr from-blue-800 to-purple-700 i justify-around items-center hidden">
+		<div>
+			<h1 class="text-white font-bold text-4xl font-sans">Contact ME</h1>
+			{/* <p class="text-white mt-1">The most popular peer to peer lending at SEA</p> */}
+			{/* <button type="submit" class="block w-28 bg-white text-indigo-800 mt-4 py-2 rounded-2xl font-bold mb-2">Read More</button> */}
+		</div>
+		<div class="absolute -bottom-32 -left-40 w-80 h-80 border-4 rounded-full border-opacity-30 border-t-8"></div>
+		<div class="absolute -bottom-40 -left-20 w-80 h-80 border-4 rounded-full border-opacity-30 border-t-8"></div>
+		<div class="absolute -top-40 -right-0 w-80 h-80 border-4 rounded-full border-opacity-30 border-t-8"></div>
+		<div class="absolute -top-20 -right-20 w-80 h-80 border-4 rounded-full border-opacity-30 border-t-8"></div>
+	</div>
+	<div class="flex md:w-1/2 justify-center py-10 items-center bg-white">
+		<form class="bg-white">
+			<h1 class="text-gray-800 font-bold text-2xl mb-1">Hello!</h1>
+			{/* <p class="text-sm font-normal text-gray-600 mb-7">Welcome Back</p> */}
+			<div class="flex items-center border-2 py-2 px-3 rounded-2xl mb-4">
+				<svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 text-gray-400" viewBox="0 0 20 20"
+					fill="currentColor">
+					<path fill-rule="evenodd" d="M10 9a3 3 0 100-6 3 3 0 000 6zm-7 9a7 7 0 1114 0H3z"
+						clip-rule="evenodd" />
+				</svg>
+				<input class="pl-2 outline-none border-none" type="text" name="" id="" placeholder="Full name" />
+      </div>
+				<div class="flex items-center border-2 py-2 px-3 rounded-2xl mb-4">
+					<svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 text-gray-400" fill="none"
+						viewBox="0 0 24 24" stroke="currentColor">
+						<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+							d="M12 11c0 3.517-1.009 6.799-2.753 9.571m-3.44-2.04l.054-.09A13.916 13.916 0 008 11a4 4 0 118 0c0 1.017-.07 2.019-.203 3m-2.118 6.844A21.88 21.88 0 0015.171 17m3.839 1.132c.645-2.266.99-4.659.99-7.132A8 8 0 008 4.07M3 15.364c.64-1.319 1-2.8 1-4.364 0-1.457.39-2.823 1.07-4" />
+					</svg>
+					<input class="pl-2 outline-none border-none" type="text" name="" id="" placeholder="phone" />
+      </div>
+					<div class="flex items-center border-2 py-2 px-3 rounded-2xl mb-4">
+						<svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 text-gray-400" fill="none"
+							viewBox="0 0 24 24" stroke="currentColor">
+							<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+								d="M16 12a4 4 0 10-8 0 4 4 0 008 0zm0 0v1.5a2.5 2.5 0 005 0V12a9 9 0 10-9 9m4.5-1.206a8.959 8.959 0 01-4.5 1.207" />
+						</svg>
+						<input class="pl-2 outline-none border-none" type="text" name="" id="" placeholder="Email Address" />
+      </div>
+						<div class="flex items-center border-2 py-2 px-3 rounded-2xl">
+							{/* <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 text-gray-400" viewBox="0 0 20 20"
+								fill="currentColor">
+								<path fill-rule="evenodd"
+									d="M5 9V7a5 5 0 0110 0v2a2 2 0 012 2v5a2 2 0 01-2 2H5a2 2 0 01-2-2v-5a2 2 0 012-2zm8-2v2H7V7a3 3 0 016 0z"
+									clip-rule="evenodd" />
+							</svg> */}
+							<input class="pl-2 outline-none border-none" type="text" name="" id="" placeholder="comment" />
+      </div>
+							<button type="submit" class="block w-full bg-indigo-600 mt-4 py-2 rounded-2xl text-white font-semibold mb-2">submit</button>
+							{/* <span class="text-sm ml-2 hover:text-blue-500 cursor-pointer">Forgot Password ?</span> */}
+		</form>
+	</div>
+</div>  
       );
 };
